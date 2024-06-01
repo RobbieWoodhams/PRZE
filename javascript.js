834f7157b717940466fe2797e6036d6c6c54c00d
RobbieWoodhams <robert.woodhams@students.plymouth.ac.uk>
2024-06-01 16:21:48 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+VANTA.GLOBE({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfff,
+    color2: 0xff,
+    backgroundColor: 0xffffff
+  });
